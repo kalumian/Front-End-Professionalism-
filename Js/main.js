c1c552fa2490b2c1fd964092dd39f2f8885a1595
kalumian <tradation10@gmail.com>
2023-02-26 02:08:37 +0300

    first commit

---
+++ Js/main.js
@@ -0,0 +1,9 @@
+// Add The logo for every pages
+function addLogoForPage() {
+  const logo = document.createElement("link");
+  logo.rel = "icon";
+  logo.href = "../images/index/icon.jpg";
+  document.head.appendChild(logo);
+}
+addLogoForPage();
+// -----------------------------
